f8ddbd5fdfa9514b403ba1986ce4a5ea8bb8cd57
Evgeniy Glazyrin <i@genhoi.ru>
2023-05-26 13:07:54 +0400

    fix: change in tables

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -2,7 +2,7 @@ create table if not exists bids (
       id           bigint       primary key
     , code         varchar(255)
     , title        bigint
-    , bid_delivery varchar(255)
+    , bid_delivery_code varchar(255)
 );
 
 create table if not exists bid_deliveries (
